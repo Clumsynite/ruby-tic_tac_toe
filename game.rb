68d64afcb972140066978a560d2f87f630254fd1
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-07-03 15:17:24 +0530

    Changed Class varialbe to instance variable
    @@board -> @board
    So that everytime a class is called A different board is created

--- game.rb
+++ game.rb
@@ -1,16 +1,16 @@
 class Game
     
-    @@board = {1=> '1', 2=> '2', 3=> '3', 4=> '4', 5=> '5', 6=> '6', 7=> '7', 8=> '8', 9=> '9'}
+    @board = {1=> '1', 2=> '2', 3=> '3', 4=> '4', 5=> '5', 6=> '6', 7=> '7', 8=> '8', 9=> '9'}
     $turn_x = true
     $turn_y = false
     $condition = []
     def printBoard
         puts "_____________"
-        puts "| #{@@board[1]} | #{@@board[2]} | #{@@board[3]} |"
+        puts "| #{@board[1]} | #{@board[2]} | #{@board[3]} |"
         puts "_____________"
-        puts "| #{@@board[4]} | #{@@board[5]} | #{@@board[6]} |"
+        puts "| #{@board[4]} | #{@board[5]} | #{@board[6]} |"
         puts "_____________"
-        puts "| #{@@board[7]} | #{@@board[8]} | #{@@board[9]} |"
+        puts "| #{@board[7]} | #{@board[8]} | #{@board[9]} |"
         puts "-------------"
     end
   
@@ -25,8 +25,8 @@ class Game
     def move(value)
         puts "Enter a value from the numeric cells to place #{value}: "
         cell = gets.chomp!.to_i
-        if @@board.has_key? cell and write_protect(cell)
-            @@board[cell]=value
+        if @board.has_key? cell and write_protect(cell)
+            @board[cell]=value
             if value=='X'
                 $turn_x = false
                 $turn_y = true
@@ -42,7 +42,7 @@ class Game
     end
     
     def write_protect(value)
-        if @@board[value] =='X' || @@board[value] =='O'
+        if @board[value] =='X' || @board[value] =='O'
             return false
         else
             return true
@@ -51,10 +51,10 @@ class Game
 
     def check_log
         @WIN = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-        $condition = @WIN.select {|i| [@@board[i[0]], @@board[i[1]], @@board[i[2]]].uniq.size == 1}
+        $condition = @WIN.select {|i| [@board[i[0]], @board[i[1]], @board[i[2]]].uniq.size == 1}
         
         if $condition.length==1
-            puts "Player #{@@board[$condition[0][1]]} Won" 
+            puts "Player #{@board[$condition[0][1]]} Won" 
             return true
         else
             return false
@@ -62,7 +62,7 @@ class Game
     end
 
     def board_full
-        x_o = @@board.select{ |x,y|  y!='X' and y!='O'}
+        x_o = @board.select{ |x,y|  y!='X' and y!='O'}
         return x_o.length>0?false:true
     end
 
